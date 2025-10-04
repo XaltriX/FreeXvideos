95a9f3440cb0d6215b835adb436fa7f2913e44ad
XaltriX <129171425+XaltriX@users.noreply.github.com>
2025-10-04 20:23:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5706788169"))
 #Port
 PORT = os.environ.get("PORT", "8030")
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://mhmdmcsd:Passw0rd1@cluster0.tuflupv.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://mrnoffice692:PsO4VGHI9heKd7WA@cluster0.o1vcj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #Time in seconds for message delete, put 0 to never delete
